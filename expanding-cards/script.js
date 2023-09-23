2663bc1c9361688432775a4e3260d31d4b5bc597
Chheang Sotheng <chheangsotheng@yahoo.com>
2023-09-23 13:32:57 -0700

    add JS for add/remove active cards in expanding-cards project

--- expanding-cards/script.js
+++ expanding-cards/script.js
@@ -1,1 +1,18 @@
-//
+const panels = document.querySelectorAll('.panel')
+
+// console.log(panels)
+// console.log(panels[0])
+
+panels.forEach(panel => {
+    // console.log(panels)
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
